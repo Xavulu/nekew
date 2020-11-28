95f4e3821bfa2d03fbce62f9a1eb32f71dfa85de
Xavier Fernandez <lain@MacBook-Pro-3.home>
2020-11-28 01:56:37 -0500

    somehow sodiumoxide compiles now :|

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## nekew 
 yet another wip from your boy xavier f. 
 see the description
+
+### debug
+```
+cargo build --color always && ./target/debug/nekew 
+```
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,65 @@
-mod error; 
+#![allow(dead_code, unused_imports)]
+
+
 mod encrypt;
 
 use colored::*;
-use clap::{Arg, App, SubCommand}; 
+use clap::{Arg, App, SubCommand, AppSettings}; 
 
+const EXTENSION: &str = ".nekew";
 
 const LOGO: &str =  r#"
             _                  
  _ __   ___| | _______      __  ／l、        
 | '_ \ / _ \ |/ / _ \ \ /\ / /ﾞ（ﾟ､ ｡ ７ 
 | | | |  __/   <  __/\ V  V /　 l、ﾞ ~ヽ  
-|_| |_|\___|_|\_\___| \_/\_/　  じしf_, )ノ  
+|_| |_|\___|_|\_\___| \_/\_/　  じしf_, )ノ 
 "#;
 
 
 
 fn main() {
-    let logotxt: ColoredString = String::from(LOGO).magenta();
+    /*let logotxt: ColoredString = String::from(LOGO).magenta();*/
     let nekew= App::new(LOGO)
-        .version("0.1.0")
-        .author("Xavier F. <xavier.fernandez92@myhunter.cuny.edu>")
-        .about("a feline themed file encryption app ₍⸍⸌̣ʷ̣̫⸍̣⸌₎")
+        .setting(AppSettings::ColorAlways)
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .version("\n0.1.0")
+        .author("Xavier F. <https://github.com/Xavulu/nekew>")
+        .about(r#"a feline themed file encryption app 🐈"#)
         .arg(Arg::with_name("encrypt")
-            .short("en")
+            .short("e")
             .long("encrypt")
             .help("takes in a file for encryption")
             .value_name("FILE")
             .takes_value(true))
         .arg(Arg::with_name("decrypt")
-            .short("de")
+            .short("d")
             .long("decrypt")
             .help("takes in a file for decryption")
             .value_name("FILE")
             .takes_value(true))
         .arg(Arg::with_name("password")
-            .short("pass")
+            .short("p")
             .long("password")
             .help("password for encryption/decryption")
             .value_name("PASS")
-            .takes_value(true))
-        .arg(Arg::with_name("scratch")
-            .short("sc")
-            .long("destroy")
-            .help("optional flag to destroy the original file after encryption")
-            .takes_value(false)
-            .required(false)
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("mangle")
+            .short("m")
+            .long("mangle")
+            .help("destroy the original file after encryption")
+            .value_name("KILL")
+            .takes_value(true)
+            .required(true)
             )
+        .arg(Arg::with_name("out")
+            .short("o")
+            .long("out")
+            .help("output directory for encrypted/decrypted file")
+            .value_name("DIR")
+            .takes_value(true)
+            .required(true))
         .get_matches();
     
 }
