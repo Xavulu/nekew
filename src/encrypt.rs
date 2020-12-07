64ca9fc18e6261f680650372f56a0e824d25cf52
Xavier Fernandez <lain@MacBook-Pro-3.home>
2020-12-07 16:19:09 -0500

    added encryption function

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -2,6 +2,8 @@
 
 use std::path::Path;
 use std::fs::File;
+use std::io::{Read, Write, Seek, SeekFrom};
+use std::{error, fmt};
 
 use paris::Logger; 
 use sodiumoxide::crypto::secretstream::{Stream, Tag, KEYBYTES, HEADERBYTES, ABYTES};
@@ -14,19 +16,90 @@ use tracing::{info, instrument};
 const MEOWGIC_NUM: [u8; 4] = [0x6E, 0x65, 0x4b, 0x77];
 const CHUNK_SIZE: usize = 4096;
 
-pub fn encrypt(in_put: &mut File, out_put: &mut File, pass: &str) -> Result<(), Report>{
-    let mut logger = Logger::new();
-    logger.loading("<blue>beginning file encryption</>  <magenta>ฅ(＾・ω・＾ฅ)</>");
+#[derive(Debug)] 
+struct NekewError { 
+    message: String,
+}
 
+impl NekewError { 
+    fn create(msg: &str) -> Self { 
+        NekewError{
+            message: msg.to_string()
+        }
+    }
+}
+
+impl fmt::Display for NekewError { 
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { 
+        write!(f, "{}", self.message)
+    }
+} 
+
+impl error::Error for NekewError {}
+
+
+
+#[instrument]
+pub fn encrypt(in_put: &mut File, out_put: &mut File, pass: &str, sens: bool) -> Result<(), Box<dyn error::Error> >{
+    
+    let mut buf = [0; CHUNK_SIZE]; 
+    let mut remainder = in_put.metadata()?.len(); 
+
+    out_put.write(&MEOWGIC_NUM)?; 
+
+    let salt = pwhash::gen_salt();
+    out_put.write(&salt.0)?; 
+
+    let mut kpass = [0u8; KEYBYTES];
+    match sens { 
+        true => pwhash::derive_key(&mut kpass, pass.as_bytes(), &salt, OPSLIMIT_SENSITIVE, MEMLIMIT_SENSITIVE).unwrap(), 
+        false => pwhash::derive_key(&mut kpass, pass.as_bytes(), &salt, OPSLIMIT_INTERACTIVE, MEMLIMIT_INTERACTIVE).unwrap(),
+    };
+    let kpass = Key(kpass); 
+    let (mut stream, header) = Stream::init_push(&kpass)
+        .map_err(|_| NekewError::create("\nfailed to initialize encryption stream (=xܫ x=)∫"))?; 
+    out_put.write(&header.0)?; 
+
+    loop { 
+        match (*in_put).read(&mut buf) { 
+            Ok(num_read) if num_read > 0 => { 
+                remainder -= num_read as u64; 
+                let tag = match remainder { 
+                    0 => Tag::Final, 
+                    _ => Tag::Message,
+                };
+                out_put.write(&stream.push(&buf[..num_read], None, tag)
+                    .map_err(|_| NekewError::create("\nFile encryption failed (=xܫ x=)∫"))?)?;
+                continue
+            } ,
+            Err(e) => Err(e)?, 
+            _ => break,
+        }
+    }
+
+    Ok(())
+} 
+
+#[instrument]
+pub fn decrypt(in_put: &mut File, out_put: &mut File, pass: &str, sens: bool)-> Result<(), Box<dyn error::Error>>{
     
 
+    print!("enc");
     Ok(())
 } 
 
-pub fn decrypt(in_put: &mut File, out_put: &mut File, pass: &str)-> Result<(), Report>{
-    let mut logger = Logger::new();
-    logger.loading("<blue>beginning file decryption</>  <magenta>ฅ(＾・ω・＾ฅ)</>");
+//this is taken directly from chloride, another rust file encryption tool
+//https://github.com/spieglt/Cloaker , this and cloaker basically served as documentation for sodiumoxide
+pub fn verify(in_put: &mut File) -> bool {
+    let fsize = in_put.metadata()
+        .expect("failed to retrieve metadata (=xܫ x=)∫")
+        .len(); 
 
+    if fsize < (MEOWGIC_NUM.len() + HEADERBYTES + SALTBYTES) as u64 { 
+        return false;
+    } 
+    let mut meow = [0u8; MEOWGIC_NUM.len()]; 
+    in_put.read_exact(&mut meow).expect("failed to read magic numbers (=xܫ x=)∫"); 
 
-    Ok(())
-} 
+    meow == MEOWGIC_NUM
+}
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@
 
 
 mod encrypt;
-use encrypt::*;
+use encrypt::{encrypt, decrypt, verify};
 
 use std::fs::{File, remove_file};
 use std::path::Path;
 use std::process::exit;
 use std::error::Error;
 use std::io;
+use std::io::prelude::*;
 use std::io::{Read, Write, Seek, SeekFrom};
+use std::time::Instant;
+
 use colored::*;
 use clap::{Arg, App, SubCommand, AppSettings}; 
 use color_eyre::eyre::{Report, Result, WrapErr};
@@ -28,7 +31,6 @@ const LOGO: &str =  r#"
 | | | |  __/   <  __/\ V  V /　 l、ﾞ ~ヽ  
 |_| |_|\___|_|\_\___| \_/\_/　  じしf_, )ノ 
 "#;
-
 
 
 #[instrument]
@@ -70,12 +72,18 @@ fn main() -> Result<(), Report> {
             .takes_value(true)
             .required(true)
             )
+        .arg(Arg::with_name("sensitive")
+            .short("s")
+            .long("sensitive")
+            .help("Use a better keygen for more sensitive data, memory intense")    
+            )
         .get_matches();
     //path and file validation
     let input = nekew.value_of("input").unwrap();
     let output =  nekew.value_of("out").unwrap();
     let infile = Path::new(input); 
     let outpath = Path::new(output);
+    let sensitive = nekew.is_present("sensitive");
 
     if infile.is_dir() || !infile.is_file() || !infile.exists(){ 
         eprintln!("{}", "Invalid input file, please specify path to a file (=xܫ x=)∫".red());
@@ -85,7 +93,7 @@ fn main() -> Result<(), Report> {
         eprintln!("{}", "Invalid output directory, please specify output path (=xܫ x=)∫".red());
         exit(1);
     } 
-
+    let _infile = infile;
     //password retrieval and validation
     let password = rpassword::prompt_password_stdout("Please enter a password with 10 or more characters: ")
         .expect("couldn't get a password"); 
@@ -102,18 +110,83 @@ fn main() -> Result<(), Report> {
 
     //encryption and associated file creation/checking stuff 
     let mode = nekew.value_of("mode").unwrap(); 
+    let mut cryptin = match File::open(&infile) { 
+        Err(why) => panic!("{} {}: {} ", "couldn't open".red(), infile.display(), why), 
+        Ok(file) => file,
+    };
+    
+    let proctime = Instant::now();
+    
     match mode { 
         "encrypt" | "ENCRYPT" | "Encrypt" => {  
+            log.loading("<blue>beginning file encryption</>  <magenta>ฅ(＾・ω・＾ฅ)</>");
+            let origin = Path::new(& _infile).file_name().unwrap().to_str().unwrap();
+            let out = format!("{}{}{}", outpath.display(), origin ,String::from(EXTENSION));
+            let outexist = format!("{}{}{}{}", outpath.display() ,String::from(EXISTS) , origin, String::from(EXTENSION));
+            let enc = Path::new(&out); //holy shit is string formatting bizzare here
+            let enc_exists = Path::new(&outexist);
+            //let mut filetest = File::create(enc_exists).unwrap(); 
 
+            match verify(&mut cryptin){ 
+                true => {
+                    eprintln!("{}", "\nthis file is already encrypted you know".red());
+                    exit(1);
+                }, 
+                false => { 
+                    if enc.exists() { 
+                        let mut f_exists = match File::create(&enc_exists) {
+                            Err(why) => panic!("{} {}: {} ", "couldn't create output ".red(), enc_exists.display(), why), 
+                            Ok(file) => file,
+                        };
+                        let exists_result = encrypt(&mut cryptin, &mut f_exists, password.as_str(), sensitive);
+                        match exists_result { 
+                            Ok(()) => println!("{}", "\nencryption succesful ₍˄·͈༝·͈˄*₎◞ ".blue()), 
+                            Err(failed) => panic!("Encryption error: {:?}", failed),
+                        }
+                    } 
+                    else { 
+                        let mut f_new = match File::create(&enc){
+                            Err(why) => panic!("{} {}: {} ", "couldn't create output ".red(), enc.display(), why), 
+                            Ok(file) => file,
+                        };  
+                        let enc_result = encrypt(&mut cryptin, &mut f_new, password.as_str(), sensitive);
+                        match enc_result { 
+                            Ok(()) => println!("{}", "\nencryption succesful ₍˄·͈༝·͈˄*₎◞ ".blue()), 
+                            Err(failed) => panic!("Encryption error: {:?}", failed),
+                        }
+                    }
+                },
+            }
+            
         }, 
         "decrypt" | "DECRYPT" | "Decrypt" => { 
-            
+            log.loading("<blue>beginning file decryption</>  <magenta>ฅ(＾・ω・＾ฅ)</>");
+            match verify(&mut cryptin){ 
+                true => { 
+                    let strip = infile.to_str().unwrap().to_string(); 
+                    let prepend = format!("{}dec_{}",String::from(output) , strip[..strip.len() - EXTENSION.len()].to_string());
+                    let dec_file = Path::new(&prepend); 
+                    let mut dec = match File::create(&dec_file){ 
+                        Err(why) => panic!("{} {}: {} ", "couldn't create output ".red(), dec_file.display(), why), 
+                        Ok(file) => file,
+                    };
+    
+                    let dec_result = decrypt(&mut cryptin, &mut dec, password.as_str(), sensitive);
+                }, 
+                false => { 
+                    eprintln!("{}", "this file is either not encrypted or encrypted by another program (=xܫ x=)∫".red()); 
+                    exit(1);
+                },
+            }
         }, 
         _ => { 
             eprintln!("{} {}", mode , "is not a valid mode sorry (=xܫ x=)∫".red()); 
             exit(1);
         },
     }
+
+    let finished = proctime.elapsed();
+    println!("\n{} {:#?}","completed in:".magenta() , finished);
 
     //confirmation for file shredding and destruction  
     let shred = nekew.value_of("kill").unwrap(); 
@@ -122,9 +195,9 @@ fn main() -> Result<(), Report> {
             log.warn("<yellow>The original file will be</> <red>PERMANENTLY</> <yellow>deleted/shredded</> (^._.^)");
             let shredded = file_shred::shred_file(infile);
             match shredded { 
-                Ok(i) => {
+                Ok(()) => {
                     println!("{} was succesfully shredded (=Φܫ Φ=)∫", infile.display()); 
-                    i //not really needed :)
+                    () //not really needed :)
                 }, 
                 Err(fail) => {
                     eprintln!("{}",fail.red()); 
@@ -132,7 +205,7 @@ fn main() -> Result<(), Report> {
                 } ,
             }
         },
-        _ => println!("no file shredding "),
+        _ => println!("\n{} ", "no file shredding".green()),
     }
 
     println!("goodbye {} {}", "<3".red().bold(), "₍˄·͈༝·͈˄*₎◞ ̑̑  (=Φܫ Φ=)∫  ฅ(＾・ω・＾ฅ)".magenta());
